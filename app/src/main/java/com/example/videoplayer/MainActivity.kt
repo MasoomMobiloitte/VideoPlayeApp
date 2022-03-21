36b3fb612d48b71d5edf3c5ec5084d37a78802e6
masoom zaid <an-masoom@indicchain.com>
2022-03-22 00:43:18 +0530

    Done all things

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.videoplayer
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
+import com.example.videoplayer.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        play video from online by uri link
+//        val uriPath = "https://videoshub.com/videos/25316664"
+//        val uri : Uri = Uri.parse(uriPath)
+//        binding.videoPlayer.setVideoURI(uri)
+//        val mediaController = MediaController(this)
+//        mediaController.setAnchorView(binding.videoPlayer)
+//        binding.videoPlayer.setMediaController(mediaController)
+//        binding.videoPlayer.start()
+
+//        play video statically by using raw materials
+//        val uriPath = "android.resource://" + packageName + "/" + R.raw.sample
+//        val uri: Uri = Uri.parse(uriPath)
+//        binding.videoPlayer.setVideoURI(uri)
+//        val mediaController = MediaController(this)
+//        mediaController.setAnchorView(binding.videoPlayer)
+//        binding.videoPlayer.setMediaController(mediaController)
+//        binding.videoPlayer.start()
     }
 }
